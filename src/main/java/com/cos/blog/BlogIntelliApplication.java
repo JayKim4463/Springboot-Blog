132c38904d4519dd1a015928a4d858ce5645f569
JayKim4463 <jm8763@nate.com>
2022-05-16 13:08:56 +0900

    test

---
+++ src/main/java/com/cos/blog/BlogIntelliApplication.java
@@ -0,0 +1,13 @@
+package com.cos.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogIntelliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogIntelliApplication.class, args);
+    }
+
+}
