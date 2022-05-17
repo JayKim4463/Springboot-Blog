22f5ff41ac1fa1b656a2d82135900e72d3957568
JayKim4463 <jm8763@nate.com>
2022-05-17 12:56:26 +0900

    Model Commit

---
+++ src/main/java/com/cos/blog/model/Board.java
@@ -0,0 +1,42 @@
+package com.cos.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @ColumnDefault("0")
+    private int count;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="userId")
+    private User userId;
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER) //FK 아님 (DB에 컬럼을 만들지 않음)
+    private List<Reply> reply;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
